e185c130a1c4210390eaae717b2498ece936ca36
Harisshabbir76 <05harisshabbir@gmail.com>
2025-08-09 07:14:08 -0700

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,21 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap
 import "./globals.css"; // Global Styles
 import Navbar from "@/components/CustomNavbar"; // Import Navbar component
 import { usePathname } from "next/navigation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const pathname = usePathname();
-  const isAuthPage = pathname === "/register" || pathname === "/login"; 
+  const isAuthPage = pathname === "/register" || pathname === "/login";
 
   return (
-    <html lang="en">
-      <body className={`${isAuthPage ? "background-gradient" : ""} ${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className={isAuthPage ? "background-gradient" : ""}>
         {!isAuthPage && <Navbar />}
         <main>{children}</main>
       </body>
